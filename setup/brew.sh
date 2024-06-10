8b483fcae85ced5ddbb2ec73de401feeb32f9cd1
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-06-11 00:48:40 +0300

    Update brew.sh

--- setup/brew.sh
+++ setup/brew.sh
@@ -9,14 +9,12 @@ sleep 1
 
 echo "Success! Brew packages are installed."
 
-brew install --cask 1password
 brew install --cask discord
 brew install --cask obsidian
 brew install --cask slack
 brew install --cask whatsapp
 brew install --cask brave-browser
 brew install --cask github
-brew install --cask rectangle
 brew install --cask topnotch
 brew install --cask zed
 brew install --cask docker
