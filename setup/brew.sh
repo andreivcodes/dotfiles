3f18237586b33662ba54afb501fc0b5d45c6af07
Andrei Voinea <8058187+andreivcodes@users.noreply.github.com>
2024-07-19 19:14:37 +0300

    updates

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,26 +1,40 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo 'eval $(/opt/homebrew/bin/brew shellenv)' >> $HOME/.zprofile
+
+source $HOME/.zprofile
+
 brew update
 brew upgrade
 
 brew install wget
 brew install volta
+brew install nixpacks
+
+source $HOME/.zprofile
 
 sleep 1
 
 echo "Success! Brew packages are installed."
 
 brew install --cask discord
-brew install --cask obsidian
 brew install --cask slack
 brew install --cask whatsapp
-brew install --cask brave-browser
-brew install --cask github
-brew install --cask zed@preview
-brew install --cask docker
-brew install --cask wifiman
-brew install --cask tableplus
-brew install --cask notion
 brew install --cask telegram
+
+brew install --cask obsidian
+brew install --cask notion
+
+brew install --cask zed@preview
+brew install --cask github
+brew install --cask docker
+brew install --cask tableplus
+brew install --cask brave-browser
+
+brew install --cask wifiman
+brew install --cask 1password
+brew install --cask macs-fan-control
+
 
 sleep 1
 
--- setup/dev.sh
+++ setup/dev.sh
@@ -1,9 +1,17 @@
+source $HOME/.zprofile
+
 # node
 volta setup
 volta install node
 volta install yarn
+volta install npm
+
+source $HOME/.zprofile
+source $HOME/.zshrc
 
 npm install -g npm-check-updates
 
 # rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+. "$HOME/.cargo/env"
